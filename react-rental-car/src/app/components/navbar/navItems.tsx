e1cf8dcde6e1686e1332af76b7f9e07897b7188b
Mauricio García <09mgr.15@gmail.com>
2021-06-09 21:41:54 -0600

    Reponsive navbar with react-burger-menu

--- react-rental-car/src/app/components/navbar/navItems.tsx
+++ react-rental-car/src/app/components/navbar/navItems.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import styled from 'styled-components'
-import tw from 'twin.macro'
+import React from "react";
+import styled, { css } from "styled-components";
+import tw from "twin.macro";
+import { slide as Menu } from "react-burger-menu";
+import { useMediaQuery } from "react-responsive";
+import { SCREENS } from "../responsive";
+import menuStyles from "./menuStyle";
 
 const ListContainer = styled.ul`
     ${tw`
         flex
         list-none
     `}
-`
+`;
 
-
-const NavItem = styled.li`
+const NavItem = styled.li<{ menu?: any }>`
     ${tw`
-        text-xs
+        text-base
         md:text-base
         text-black
         font-medium
@@ -24,10 +27,42 @@ const NavItem = styled.li`
         ease-in-out
         hover:text-gray-700
     `}
-`
+    ${({ menu }) =>
+        menu &&
+        css`
+            ${tw`
+                text-white
+                text-xl
+                mb-3
+                focus:text-white
+            `}
+        `}
+`;
 
 export function NavItems() {
-    return(
+    const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
+    if (isMobile)
+        return (
+            <Menu right styles={menuStyles}>
+                <ListContainer >
+                    <NavItem menu>
+                        <a href="#">Home</a>
+                    </NavItem>
+                    <NavItem menu>
+                        <a href="#">Cars</a>
+                    </NavItem>
+                    <NavItem menu>
+                        <a href="#">Services</a>
+                    </NavItem>
+                    <NavItem menu>
+                        <a href="#">Contact us</a>
+                    </NavItem>
+                </ListContainer>
+            </Menu>
+        );
+
+    return (
         <ListContainer>
             <NavItem>
                 <a href="#">Home</a>
@@ -42,5 +77,5 @@ export function NavItems() {
                 <a href="#">Contact us</a>
             </NavItem>
         </ListContainer>
-    )
+    );
 }
