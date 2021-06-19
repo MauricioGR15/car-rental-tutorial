7f72f4f2a46244ffabcbb4b8bc4492078faaf054
Mauricio García <09mgr.15@gmail.com>
2021-06-18 18:59:00 -0600

    Carousel responsive

--- react-rental-car/src/app/containers/HomePage/topCars.tsx
+++ react-rental-car/src/app/containers/HomePage/topCars.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import styled from 'styled-components'
-import tw from 'twin.macro'
-import { ICar } from '../../../typings/car'
-import { Car } from '../../components/car'
+import React, { useState } from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { ICar } from "../../../typings/car";
+import { Car } from "../../components/car";
+import Carousel, { Dots, slidesToShowPlugin } from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
+import { useMediaQuery } from "react-responsive";
+import { SCREENS } from "../../components/responsive";
 
 const TopCarsContainer = styled.div`
     ${tw`
@@ -16,7 +20,7 @@ const TopCarsContainer = styled.div`
         md:p-0
         mb-10
     `}
-`
+`;
 
 const Title = styled.h2`
     ${tw`
@@ -25,7 +29,7 @@ const Title = styled.h2`
         text-black
         font-extrabold
     `}
-`
+`;
 
 const CarsContainer = styled.div`
     ${tw`
@@ -36,39 +40,91 @@ const CarsContainer = styled.div`
         mt-7
         md:mt-10
     `}
-`
+`;
 
 const testCar: ICar = {
     name: "Audi S3 Car",
     mileage: "10k",
     thumbnailSrc:
-      "https://cdn.jdpower.com/Models/640x480/2017-Audi-S3-PremiumPlus.jpg",
+        "https://cdn.jdpower.com/Models/640x480/2017-Audi-S3-PremiumPlus.jpg",
     dailyPrice: 70,
     monthlyPrice: 1600,
     gearType: "Auto",
     gas: "Petrol",
-  };
+};
 
-  const testCar2: ICar = {
+const testCar2: ICar = {
     name: "HONDA cITY 5 Seater Car",
     mileage: "20k",
     thumbnailSrc:
-      "https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg",
+        "https://shinewiki.com/wp-content/uploads/2019/11/honda-city.jpg",
     dailyPrice: 50,
     monthlyPrice: 1500,
     gearType: "Auto",
     gas: "Petrol",
-  };
+};
 
 export function TopCars() {
+    const [current, setCurrent] = useState(0);
+
+    const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
+    const cars = [
+        <Car {...testCar} />,
+        <Car {...testCar2} />,
+        <Car {...testCar} />,
+        <Car {...testCar2} />,
+        <Car {...testCar2} />,
+    ];
+
+    const numberOfDots = isMobile ? cars.length : Math.ceil(cars.length / 3);
+
     return (
         <TopCarsContainer>
             <Title>Explore Our Top Deals</Title>
             <CarsContainer>
-                <Car {...testCar } />
-                <Car {...testCar } />
-                <Car {...testCar2 } />
+                <Carousel
+                    value={current}
+                    onChange={setCurrent}
+                    slides={cars}
+                    plugins={[
+                        "clickToChange",
+                        {
+                            resolve: slidesToShowPlugin,
+                            options: {
+                                numberOfSlides: 3,
+                            },
+                        },
+                    ]}
+                    breakpoints={{
+                        640: {
+                            plugins: [
+                                {
+                                    resolve: slidesToShowPlugin,
+                                    options: {
+                                        numberOfSlides: 1,
+                                    },
+                                },
+                            ],
+                        },
+                        900: {
+                            plugins: [
+                                {
+                                    resolve: slidesToShowPlugin,
+                                    options: {
+                                        numberOfSlides: 2,
+                                    },
+                                },
+                            ],
+                        },
+                    }}
+                />
+                <Dots
+                    value={current}
+                    onChange={setCurrent}
+                    number={numberOfDots}
+                />
             </CarsContainer>
         </TopCarsContainer>
-    )
+    );
 }
